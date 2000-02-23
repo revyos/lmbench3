77430f08ec819058ebfa93c5a4f555faece7be22
hp.com!staelin <hp.com!staelin@9cc621ae-7264-4df2-8f7e-c588375cf588>
2000-02-23 09:15:50 +0000

    Update to the new result_t structure.
    (Logical change 1.6)

--- trunk/lmbench3/src/mhz.c
+++ trunk/lmbench3/src/mhz.c
@@ -370,7 +370,7 @@ compute_mhz(result_t *r)
 		for (subset = 0, ntests = 0; subset < (1<<NTESTS); ++subset) {
 			for (j = 0, n = 0; j < NTESTS; ++j)
 				if (BIT_SET(subset, j) && r[j].N > TRIES/2)
-					data[n++] = r[j].u[r[j].N-1-i] / (double)r[j].n[r[j].N-1-i];
+					data[n++] = r[j].v[r[j].N-1-i].u / (double)r[j].v[r[j].N-1-i].n;
 			n = filter_data(data, n);
 			if (n < 2 || classes(data, n) < 2) continue;
 			results[ntests++] = 1.0 / gcd(data, n);
