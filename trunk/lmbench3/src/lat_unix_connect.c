1f842698bd2a22bcef3f8147e368896de4e86ac3
4!brucec <4!brucec@9cc621ae-7264-4df2-8f7e-c588375cf588>
2000-03-10 23:02:29 +0000

    Start on changes so test can be run in parallel "-P <parallelism>"
    (Logical change 1.13)

--- trunk/lmbench3/src/lat_unix_connect.c
+++ trunk/lmbench3/src/lat_unix_connect.c
@@ -3,8 +3,8 @@
  *
  * Three programs in one -
  *	server usage:	lat_connect -s
- *	client usage:	lat_connect hostname [N]
- *	shutdown:	lat_connect -hostname
+ *	client usage:	lat_connect [-P <parallelism>]
+ *	shutdown:	lat_connect -q
  *
  * Copyright (c) 1994 Larry McVoy.  Distributed under the FSF GPL with
  * additional restriction that results may published only if
@@ -15,68 +15,80 @@
 char	*id = "$Id$\n";
 #include "bench.h"
 
-void	server_main(int ac, char **av);
-void	client_main(int ac, char **av);
+#define CONNAME "/tmp/af_unix"
 
-void
-benchmark(uint64 iterations, void* cookie)
+void server_main(void);
+
+void benchmark(uint64 iterations, void* cookie)
 {
 	while (iterations-- > 0) {
-		int	sock = unix_connect("/tmp/af_unix");
+		int	sock = unix_connect(CONNAME);
+		if (sock <= 0)
+			printf("error on iteration %lu\n",iterations);
 		close(sock);
 	}
 }
 
-int
-main(int ac, char **av)
+int main(int ac, char **av)
 {
-	if (ac > 1 && !strcmp(av[1], "-s")) {
-		if (fork() == 0) {
-			server_main(ac, av);
+	int parallel = 1;
+	char	*usage = "-s\n OR [-P <parallelism>]\n OR -q\n";
+	char	c;
+
+	/* Start the server "-s" or Shut down the server "-q" */
+	if (ac == 2) {
+		if (!strcmp(av[1], "-s")) {
+			if (fork() == 0) {
+				server_main();
+			}
+			exit(0);
 		}
-		exit(0);
-	} else {
-		client_main(ac, av);
-	}
-	exit(0);
-	/* NOTREACHED */
-}
-
-void
-client_main(int ac, char **av)
-{
-	if (ac != 1) {
-		fprintf(stderr, "usage: %s\n", av[0]);
-		exit(1);
+		if (!strcmp(av[1], "-q")) {
+			int sock = unix_connect(CONNAME);
+			write(sock, "0", 1);
+			close(sock);
+			exit(0);
+		}
 	}
 
-	benchmp(NULL, benchmark, NULL, 0, 1, NULL);
+	/*
+	 * Rest is client
+	 */
+	while (( c = getopt(ac, av, "P:")) != EOF) {
+		switch(c) {
+		case 'P':
+			parallel = atoi(optarg);
+			if (parallel <= 0) lmbench_usage(ac, av, usage);
+			break;
+		default:
+			lmbench_usage(ac, av, usage);
+			break;
+		}
+	}
+
+	if (optind != ac) {
+		lmbench_usage(ac, av, usage);
+	}
+
+	benchmp(NULL, benchmark, NULL, 0, parallel, NULL);
 	micro("UNIX connection cost ", get_n());
-	exit(0);
-	/* NOTREACHED */
 }
 
-void
-server_main(int ac, char **av)
+void server_main(void)
 {
 	int     newsock, sock;
 	char	c;
 
-	if (ac != 2) {
-		fprintf(stderr, "usage: %s -s\n", av[0]);
-		exit(1);
-	}
 	GO_AWAY;
-	sock = unix_server("/tmp/af_unix");
+	sock = unix_server(CONNAME);
 	for (;;) {
 		newsock = unix_accept(sock);
 		c = 0;
 		read(newsock, &c, 1);
 		if (c && c == '0') {
-			unix_done(sock, "/tmp/af_unix");
+			unix_done(sock, CONNAME);
 			exit(0);
 		}
 		close(newsock);
 	}
-	/* NOTREACHED */
 }
