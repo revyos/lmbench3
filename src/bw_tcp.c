680f8eb4d7de765ffaaeb9a69430f071ad770d23
16!brucec <16!brucec@9cc621ae-7264-4df2-8f7e-c588375cf588>
2000-03-10 23:02:26 +0000

    Make changes so test can be run in parallel "-P <parallelism>"
    (Logical change 1.13)

--- src/bw_tcp.c
+++ src/bw_tcp.c
@@ -3,7 +3,7 @@
  *
  * Three programs in one -
  *	server usage:	bw_tcp -s
- *	client usage:	bw_tcp hostname
+ *	client usage:	bw_tcp [-P <parallelism>] hostname [bytes]
  *	shutdown:	bw_tcp -hostname
  *
  * Copyright (c) 1994 Larry McVoy.  Distributed under the FSF GPL with
@@ -15,21 +15,123 @@
 char	*id = "$Id$\n";
 #include "bench.h"
 
-int	server_main(int ac, char **av);
-int	client_main(int ac, char **av);
-void	source(int data);
+typedef struct _state {
+	uint64	move;
+	char	server[256];
+	int	fd;
+	char	*buf;
+} state_t;
 
-char	*buf;
+void	server_main(state_t *state);
+void	client_main(int parallel, state_t *state);
+void	source(int data, state_t *state);
+void	transfer(uint64 move, char *server, char *buf);
 
-void
-transfer(uint64 move, char *server)
+void	loop_transfer(uint64 iter, void *cookie);
+
+int main(int ac, char **av)
+{
+	int parallel = 1;
+	state_t state;
+	char	*usage = "-s\n OR [-P <parallelism>] server [size]\n OR [-]serverhost\n";
+	char	*server;
+	char	c;
+	uint64	usecs;
+	
+	state.buf = valloc(XFERSIZE);
+	touch(state.buf, XFERSIZE);
+
+	/* Start the server */
+	if (!strcmp(av[1], "-s")) {
+		if (fork() == 0) {
+			server_main(&state);
+		}
+		exit(0);
+	}
+
+	/* Shut down the server */
+	if ((ac == 2) && (av[1][0] == '-')) {
+		int	conn;
+		server = &av[1][1];
+		conn = tcp_connect(server, TCP_DATA, SOCKOPT_NONE);
+		write(conn, "0", 1);
+		exit(0);
+	}
+	
+		
+	/* Rest is client argument processing */
+	while (( c = getopt(ac, av, "P:")) != EOF) {
+		switch(c) {
+		case 'P':
+			parallel = atoi(optarg);
+			if (parallel <= 0) lmbench_usage(ac, av, usage);
+			break;
+		default:
+			lmbench_usage(ac, av, usage);
+			break;
+		}
+	}
+
+	/*
+	 * Client should have one, possibly two [indicates size] arguments left
+	 */
+	state.move = 10*1024*1024;
+	if (optind + 2 == ac) {
+		state.move = bytes(av[optind+1]);
+		strcpy(state.server,av[optind]);
+	} else if (optind + 1 == ac) {
+		strcpy(state.server,av[optind]);
+	} else {
+		lmbench_usage(ac, av, usage);
+	}
+
+	if (!state.buf) {
+		perror("valloc");
+		exit(1);
+	}
+
+	/*
+	 * Make one run take at least 5 seconds.
+	 * This minimizes the effect of connect & reopening TCP windows.
+	 */
+	start(0);
+	transfer(state.move, state.server, state.buf);
+	usecs = stop(0,0);
+	if (usecs >= LONGER) {	/* must be 10Mbit ether or sloooow box */
+		save_n(1);
+		if (parallel == 1)
+			goto out; /* can't skip next step for MP tests! */
+	}
+	usecs = 5000000 / usecs;
+	state.move  *= usecs * 1.25;
+
+	benchmp(NULL, loop_transfer, NULL,
+		LONGER, parallel, &state );
+
+out:	(void)fprintf(stderr, "Socket bandwidth using %s: ", state.server);
+	mb(state.move * get_n() * parallel);
+}
+
+void loop_transfer(uint64 iterations, void *cookie)
+{
+	state_t *state = (state_t *) cookie;
+	uint64	move = state->move;
+	char	*server = state->server;
+	char	*buf    = state->buf;
+
+	while (iterations-- > 0) {
+		transfer(move,server,buf);
+	}
+}
+
+void transfer(uint64 move, char *server, char *buf)
 {
 	int	data, c;
 	uint64	todo;
 
 	todo = move;
-	/*printf("Move %lu MB\n", (unsigned long)(move>>20));
-	*/
+	/*printf("Move %lu MB\n", (unsigned long)(move>>20)); */
+	
 	data = tcp_connect(server, TCP_DATA, SOCKOPT_READ);
 	(void)sprintf(buf, "%lu", (unsigned long)move);
 	if (write(data, buf, strlen(buf)) != strlen(buf)) {
@@ -42,72 +144,13 @@ transfer(uint64 move, char *server)
 	(void)close(data);
 }
 
-/* ARGSUSED */
-int
-client_main(int ac, char **av)
-{
-	uint64	move;
-	char   *server;
-	uint64	usecs;
-
-	if (ac != 2 && ac != 3) {
-		(void)fprintf(stderr, "usage: %s remotehost [bytes]\n",
-		    av[0]);
-		exit(0);
-	}
-	if (!buf) {
-		perror("valloc");
-		exit(1);
-	}
-	if (ac == 3) {
-		move = bytes(av[2]);
-	} else {
-		move = 10*1024*1024;
-	}
-	/*
-	 * Disabler message to other side.
-	 */
-	if (av[1][0] == '-') {
-		int	data;
-
-		move = 0;
-		server = &av[1][1];
-		data = tcp_connect(server, TCP_DATA, SOCKOPT_NONE);
-		write(data, "0", 1);
-		exit(0);
-	} else {
-		server = av[1];
-	}
-	/*
-	 * Make one run take at least 5 seconds.
-	 * This minimizes the effect of connect & reopening TCP windows.
-	 */
-	start(0);
-	transfer(move, server);
-	usecs = stop(0,0);
-	if (usecs >= LONGER) {	/* must be 10Mbit ether or sloooow box */
-		save_n(1);
-		goto out;
-	}
-	usecs = 5000000 / usecs;
-	move *= usecs * 1.25;
-	BENCH(transfer(move, server), LONGER);
-out:	(void)fprintf(stderr, "Socket bandwidth using %s: ", server);
-	mb(move * get_n());
-	exit(0);
-	/*NOTREACHED*/
-}
-
-void
-child()
+void child()
 {
 	wait(0);
 	signal(SIGCHLD, child);
 }
 
-/* ARGSUSED */
-int
-server_main(int ac, char **av)
+void server_main(state_t *state)
 {
 	int	data, newdata;
 
@@ -123,7 +166,7 @@ server_main(int ac, char **av)
 			perror("fork");
 			break;
 		    case 0:
-			source(newdata);
+			source(newdata, state);
 			exit(0);
 		    default:
 			close(newdata);
@@ -136,21 +179,20 @@ server_main(int ac, char **av)
  * Read the number of bytes to be transfered.
  * Write that many bytes on the data socket.
  */
-void
-source(int data)
+void source(int data, state_t *state)
 {
 	int	n, nbytes;
 
-	if (!buf) {
+	if (!state->buf) {
 		perror("valloc");
 		exit(1);
 	}
-	bzero((void*)buf, XFERSIZE);
-	if (read(data, buf, XFERSIZE) <= 0) {
+	bzero((void*)state->buf, XFERSIZE);
+	if (read(data, state->buf, XFERSIZE) <= 0) {
 		perror("control nbytes");
 		exit(7);
 	}
-	nbytes = atoi(buf);
+	nbytes = atoi(state->buf);
 
 	/*
 	 * A hack to allow turning off the absorb daemon.
@@ -162,32 +204,10 @@ source(int data)
 	}
 	while (nbytes > 0) {
 #ifdef	TOUCH
-		touch(buf, XFERSIZE);
+		touch(state->buf, XFERSIZE);
 #endif
-		n = write(data, buf, XFERSIZE);
+		n = write(data, state->buf, XFERSIZE);
 		if (n <= 0) break;
 		nbytes -= n;
 	}
-}
-
-
-int
-main(int ac, char **av)
-{
-	if (ac != 2 && ac != 3) {
-		fprintf(stderr, "Usage: %s -s OR %s [-]serverhost\n",
-		    av[0], av[0]);
-		exit(1);
-	}
-	buf = valloc(XFERSIZE);
-	touch(buf, XFERSIZE);
-	if (!strcmp(av[1], "-s")) {
-		if (fork() == 0) {
-			server_main(ac, av);
-		}
-		exit(0);
-	} else {
-		client_main(ac, av);
-	}
-	return(0);
 }
