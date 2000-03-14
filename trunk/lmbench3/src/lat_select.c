1cbc7ba7b03aa84616874d1c06644ee2e925f6c2
10!brucec <10!brucec@9cc621ae-7264-4df2-8f7e-c588375cf588>
2000-03-14 23:03:59 +0000

    First pass at making this test MP "-P <parallelism>" ready (not working yet)
    (Logical change 1.14)

--- trunk/lmbench3/src/lat_select.c
+++ trunk/lmbench3/src/lat_select.c
@@ -1,7 +1,7 @@
 /*
  * lat_select.c - time select system call
  *
- * usage: lat_select [n]
+ * usage: lat_select [-P <parallelism>] [n]
  *
  * Copyright (c) 1996 Larry McVoy.  Distributed under the FSF GPL with
  * additional restriction that results may published only if
@@ -12,35 +12,88 @@ char	*id = "$Id$\n";
 
 #include "bench.h"
 
-void
-doit(int n, fd_set *set)
-{
-	fd_set	nosave = *set;
-	static	struct timeval tv;
-	select(n, 0, &nosave, 0, &tv);
-}
+void initialize(void *cookie);
+void cleanup(void *cookie);
+void doit(uint64 iterations,void *cookie);
+void writer(int w, int r);
 
-int
-main(int ac, char **av)
+typedef struct _state {
+	int	num;
+	fd_set  set;
+} state_t;
+
+int main(int ac, char **av)
 {
-	int	i, last = 0 /* lint */;
-	int	N = 200, fd;
-	fd_set	set;
+	state_t state;
+	int parallel = 1;
+	int c;
+	char* usage = "[-P <parallelism>]\n";
 	char	buf[256];
 
-	morefds();
-	if (ac > 1) N = atoi(av[1]);
-	FD_ZERO(&set);
+	morefds();  /* bump fd_cur to fd_max */
+	while (( c = getopt(ac, av, "P:")) != EOF) {
+		switch(c) {
+		case 'P':
+			parallel = atoi(optarg);
+			if (parallel <= 0) lmbench_usage(ac, av, usage);
+			break;
+		default:
+			lmbench_usage(ac, av, usage);
+			break;
+		}
+	}
+	state.num = 200;
+	if (optind + 1 == ac) {
+		state.num = atoi(av[optind]);
+		puts("num bumped");
+	} else if (optind < ac) {
+		lmbench_usage(ac, av, usage);
+	}
+
+	benchmp(initialize, doit, cleanup, 0, parallel, &state);
+	sprintf(buf, "Select on %d fd's", state.num);
+	micro(buf, get_n());
+}
+
+
+void doit(uint64 iterations, void * cookie)
+{
+	state_t * 	state = (state_t *)cookie;
+	register int 	n = state->num;
+	fd_set	nosave = state->set;
+	static	struct timeval tv;
+	static count = 0;
+	
+
+	if (!(count++ % 100))
+		printf("doit with iterations %d\n", (int) iterations);
+	while (iterations-- > 0) {
+		select(n, 0, &nosave, 0, &tv);
+	}
+}
+
+void initialize(void *cookie)
+{
+	char	c;
+	state_t * state = (state_t *)cookie;
+
+	int	i, last = 0 /* lint */;
+	int	N = state->num, fd;
+
+	FD_ZERO(&(state->set));
 	for (i = 3; i < 50; ++i) close(i);
 	for (fd = 0; fd < N; fd++) {
 		i = dup(0);
 		if (i == -1) break;
 		last = i;
-		FD_SET(i, &set);
+		FD_SET(i, &(state->set));
 	}
-	last++;
-	BENCH(doit(last, &set), 0);
-	sprintf(buf, "Select on %d fd's", fd);
-	micro(buf, get_n());
-	return(0);
 }
+
+void cleanup(void *cookie)
+{
+	char	c;
+	state_t * state = (state_t *)cookie;
+}
+
+	     
