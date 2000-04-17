440576956d4dc6d857706a0ccecb7cef7472a0f0
hp.com!staelin <hp.com!staelin@9cc621ae-7264-4df2-8f7e-c588375cf588>
2000-04-17 11:06:12 +0000

    - debugged a deadlock in the benchmark (wrong execution order in procB) - rewrote the benchmark to fit the new benchmp timing harness - added getopt() parameter handling
    (Logical change 1.16)

--- trunk/lmbench3/src/lat_fcntl.c
+++ trunk/lmbench3/src/lat_fcntl.c
@@ -15,7 +15,6 @@ char	*id = "$Id: lat_pipe.c,v 1.8 1997/06/16 05:38:58 lm Exp $\n";
 
 struct	flock lock, unlock;
 struct	flock s1, s2;
-int	fd1, fd2;
 
 /*
  * Create two files, use them as a ping pong test.
@@ -33,98 +32,171 @@ int	fd1, fd2;
 #define	waiton(fd)	fcntl(fd, F_SETLKW, &lock)
 #define	release(fd)	fcntl(fd, F_SETLK, &unlock)
 
-void
-procA()
-{
-	if (waiton(fd1) == -1) {
-		perror("lock of fd1 failed\n");
-		exit(1);
-	}
-	if (release(fd2) == -1) {
-		perror("unlock of fd2 failed\n");
-		exit(1);
-	}
-	if (waiton(fd2) == -1) {
-		perror("lock of fd2 failed\n");
-		exit(1);
-	}
-	if (release(fd1) == -1) {
-		perror("unlock of fd1 failed\n");
-		exit(1);
-	}
-}
-
-void
-procB()
-{
-	if (release(fd1) == -1) {
-		perror("unlock of fd1 failed\n");
-		exit(1);
-	}
-	if (waiton(fd2) == -1) {
-		perror("lock of fd2 failed\n");
-		exit(1);
-	}
-	if (waiton(fd1) == -1) {
-		perror("lock of fd1 failed\n");
-		exit(1);
-	}
-	if (release(fd2) == -1) {
-		perror("unlock of fd2 failed\n");
-		exit(1);
-	}
-}
-
-int
-main()
-{
-	char	buf[10000];
+struct _state {
+	char filename1[2048];
+	char filename2[2048];
 	int	pid;
+	int	fd1;
+	int	fd2;
+};
 
-	unlink("/tmp/lmbench-fcntl.1");
-	unlink("/tmp/lmbench-fcntl.2");
-	if ((fd1 = open("/tmp/lmbench-fcntl.1", O_CREAT|O_RDWR, 0666)) == -1) {
+void initialize(void* cookie);
+void benchmark(uint64 iterations, void* cookie);
+void cleanup(void* cookie);
+
+void
+procA(struct _state *state)
+{
+	if (waiton(state->fd1) == -1) {
+		perror("lock of fd1 failed\n");
+		cleanup(state);
+		exit(1);
+	}
+	if (release(state->fd2) == -1) {
+		perror("unlock of fd2 failed\n");
+		cleanup(state);
+		exit(1);
+	}
+	if (waiton(state->fd2) == -1) {
+		perror("lock of fd2 failed\n");
+		cleanup(state);
+		exit(1);
+	}
+	if (release(state->fd1) == -1) {
+		perror("unlock of fd1 failed\n");
+		cleanup(state);
+		exit(1);
+	}
+}
+
+void
+procB(struct _state *state)
+{
+	if (release(state->fd1) == -1) {
+		perror("unlock of fd1 failed\n");
+		cleanup(state);
+		exit(1);
+	}
+	if (waiton(state->fd2) == -1) {
+		perror("lock of fd2 failed\n");
+		cleanup(state);
+		exit(1);
+	}
+	if (release(state->fd2) == -1) {
+		perror("unlock of fd2 failed\n");
+		cleanup(state);
+		exit(1);
+	}
+	if (waiton(state->fd1) == -1) {
+		perror("lock of fd1 failed\n");
+		cleanup(state);
+		exit(1);
+	}
+}
+
+void 
+initialize(void* cookie)
+{
+	int	pid;
+	char	buf[10000];
+	struct _state* state = (struct _state*)cookie;
+
+	sprintf(state->filename1, "/tmp/lmbench-fcntl%d.1", getpid());
+	sprintf(state->filename2, "/tmp/lmbench-fcntl%d.2", getpid());
+
+	unlink(state->filename1);
+	unlink(state->filename2);
+	if ((state->fd1 = open(state->filename1, O_CREAT|O_RDWR, 0666)) == -1) {
 		perror("create");
 		exit(1);
 	}
-	if ((fd2 = open("/tmp/lmbench-fcntl.2", O_CREAT|O_RDWR, 0666)) == -1) {
+	if ((state->fd2 = open(state->filename2, O_CREAT|O_RDWR, 0666)) == -1) {
 		perror("create");
 		exit(1);
 	}
-	write(fd1, buf, sizeof(buf));
-	write(fd2, buf, sizeof(buf));
+	write(state->fd1, buf, sizeof(buf));
+	write(state->fd2, buf, sizeof(buf));
 	lock.l_type = F_WRLCK;
 	lock.l_whence = 0;
 	lock.l_start = 0;
 	lock.l_len = 1;
 	unlock = lock;
 	unlock.l_type = F_UNLCK;
-	if (waiton(fd1) == -1) {
+	if (waiton(state->fd1) == -1) {
 		perror("lock1");
 		exit(1);
 	}
-	if (waiton(fd2) == -1) {
+	if (waiton(state->fd2) == -1) {
 		perror("lock2");
 		exit(1);
 	}
-	if (fork()) {
-		wait(0);
-	}
-	pid = fork();
-	if (pid == -1) {
+	state->pid = getpid();
+	switch (pid = fork()) {
+	case -1:
 		perror("fork");
 		exit(1);
-	}
-	if (pid > 0) {
-		waiton(fd1);
-		BENCH(procA(), SHORT);
-		micro("Fcntl lock latency", get_n());
-		kill(pid, 15);
-	} else {
-		waiton(fd2);
+	case 0:
 		for ( ;; ) {
-			procB();
+			procB(state);
+		}
+		exit(0);
+	default:
+		state->pid = pid;
+		break;
+	}
+}
+
+void
+benchmark(uint64 iterations, void* cookie)
+{
+	struct _state* state = (struct _state*)cookie;
+	uint64	i;
+	
+	for (i = 0; i < iterations; ++i) {
+		procA(state);
+	}
+}
+
+void cleanup(void* cookie)
+{
+	int i;
+	struct _state* state = (struct _state*)cookie;
+
+	close(state->fd1);
+	close(state->fd2);
+
+	unlink(state->filename1);
+	unlink(state->filename2);
+
+	kill(state->pid, 9);
+}
+
+int
+main(int ac, char **av)
+{
+	int	i;
+	int	c;
+	int	parallel = 1;
+	struct _state state;
+	char *usage = "[-P <parallelism>]\n";
+
+	/*
+	 * If they specified a parallelism level, get it.
+	 */
+	while (( c = getopt(ac, av, "P:")) != EOF) {
+		switch(c) {
+		case 'P':
+			parallel = atoi(optarg);
+			if (parallel <= 0) lmbench_usage(ac, av, usage);
+			break;
+		default:
+			lmbench_usage(ac, av, usage);
+			break;
 		}
 	}
+
+	benchmp(initialize, benchmark, cleanup, 0, parallel, &state);
+	micro("Fcntl lock latency", get_n());
+
 	return (0);
 }
