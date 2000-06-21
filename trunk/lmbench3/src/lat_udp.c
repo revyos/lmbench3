cd5436f592ec61f20a2a405675faa02f1d846c5d
hp.com!staelin <hp.com!staelin@9cc621ae-7264-4df2-8f7e-c588375cf588>
2000-06-21 16:28:58 +0000

    - Cleaned up command line parsing
    (Logical change 1.18)

--- trunk/lmbench3/src/lat_udp.c
+++ trunk/lmbench3/src/lat_udp.c
@@ -16,7 +16,7 @@ char	*id = "$Id$\n";
 #include "bench.h"
 
 void	client_main(int ac, char **av);
-void	server_main();
+void	server_main(int msize);
 void	init(void *cookie);
 void	cleanup(void *cookie);
 void    doit(uint64 iter, void *cookie);
@@ -24,9 +24,91 @@ void    doit(uint64 iter, void *cookie);
 typedef struct _state {
 	int	sock;
 	int	seq;
+	int	msize;
 	char	*server;
+	char	*buf;
 } state_t;
 
+
+int main(int ac, char **av)
+{
+	state_t state;
+	int	c;
+	int	parallel = 1;
+	int	server = 0;
+	int	shutdown = 0;
+	int	msize = 4;
+ 	char	buf[256];
+	char	*usage = "-s\n OR [-S] [-P <parallelism>] server\n NOTE: message size must be >= 4\n";
+
+	if (sizeof(int) != 4) {
+		fprintf(stderr, "lat_udp: Wrong sequence size\n");
+		return(1);
+	}
+
+	while (( c = getopt(ac, av, "sSm:P:")) != EOF) {
+		switch(c) {
+		case 's': /* Server */
+			server = 1;
+			break;
+		case 'S': /* shutdown serverhost */
+		{
+			int seq, n;
+			int sock = udp_connect(av[optind],
+					       UDP_XACT,
+					       SOCKOPT_NONE);
+			for (n = -1; n > -5; --n) {
+				seq = htonl(n);
+				(void) send(sock, &seq, sizeof(int), 0);
+			}
+			close(sock);
+			exit (0);
+		}
+		case 'm':
+			msize = atoi(optarg);
+			if (msize < sizeof(int)) {
+				lmbench_usage(ac, av, usage);
+				msize = 4;
+			}
+			break;
+		case 'P':
+			parallel = atoi(optarg);
+			if (parallel <= 0)
+				lmbench_usage(ac, av, usage);
+			break;
+		default:
+			lmbench_usage(ac, av, usage);
+			break;
+		}
+	}
+
+	if (server) {
+		if (fork() == 0) {
+			server_main(msize);
+		}
+		exit(0);
+	}
+
+	if (optind + 1 != ac) {
+		lmbench_usage(ac, av, usage);
+	}
+
+	state.server = av[optind];
+	state.msize = msize;
+	benchmp(init, doit, cleanup, SHORT, parallel, &state);
+	sprintf(buf, "UDP latency using %s", state.server);
+	micro(buf, get_n());
+	exit(0);
+}
+
+void init(void * cookie)
+{
+	state_t *state = (state_t *) cookie;
+
+	state->sock = udp_connect(state->server, UDP_XACT, SOCKOPT_NONE);
+	state->seq = 0;
+	state->buf = (char*)malloc(state->msize);
+}
 
 void doit(uint64 iterations, void *cookie)
 {
@@ -36,14 +118,13 @@ void doit(uint64 iterations, void *cookie)
 	int sock = state->sock;
 	int ret;
 
-
 	while (iterations-- > 0) {
-		seq++;
-		if (send(sock, &net, sizeof(net), 0) != sizeof(net)) {
+		*(int*)state->buf = htonl(seq++);
+		if (send(sock, state->buf, state->msize, 0) != state->msize) {
 			perror("lat_udp client: send failed");
 			exit(5);
 		}
-		if (recv(sock, &ret, sizeof(ret), 0) != sizeof(ret)) {
+		if (recv(sock, state->buf, state->msize, 0) != state->msize) {
 			perror("lat_udp client: recv failed");
 			exit(5);
 		}
@@ -51,89 +132,19 @@ void doit(uint64 iterations, void *cookie)
 	state->seq = seq;
 }
 
-int main(int ac, char **av)
-{
-	state_t state;
-	int parallel = 1;
- 	char	buf[256];
-	char	*usage = "-s\n OR [-P <parallelism>] server\n OR [-]serverhost\n";
-	if (sizeof(int) != 4) {
-		fprintf(stderr, "lat_udp: Wrong sequence size\n");
-		return(1);
-	}
-	if (ac == 1)
-		lmbench_usage(ac, av, usage);
-	if (!strcmp(av[1], "-s")) {
-		if (fork() == 0) {
-			server_main();
-		}
-		return(0);
-	} else { /*
-		  * Client args are -server OR [-P <parallelism>] server
-		  */
-		int c;
-		if (ac == 2) {
-			if (!strcmp(av[1],"-"))
-				lmbench_usage(ac, av, usage);
-			if (av[1][0] == '-') { /* shut down server */
-				int seq = -1;
-				int sock = udp_connect(&av[1][1],
-						       UDP_XACT,
-						       SOCKOPT_NONE);
-				while (seq-- > -5) {
-					int	net = htonl(seq);
-					(void) send(sock, &net,
-						    sizeof(net), 0);
-				}
-				close(sock);
-				exit(0);
-			}
-		}
-
-		while (( c = getopt(ac, av, "P:")) != EOF) {
-			switch(c) {
-			case 'P':
-				parallel = atoi(optarg);
-				if (parallel <= 0)
-					lmbench_usage(ac, av, usage);
-				break;
-			default:
-				lmbench_usage(ac, av, usage);
-				break;
-			}
-		}
-
-		if (optind + 1 != ac) {
-			lmbench_usage(ac, av, usage);
-		}
-		state.server = av[optind];
-	}
-
-	benchmp(init, doit, cleanup,
-		SHORT, parallel, &state);
-	sprintf(buf, "UDP latency using %s", state.server);
-	micro(buf, get_n());
-}
-
-void init(void * cookie)
-{
-	state_t *state = (state_t *) cookie;
-
-	state->sock = udp_connect(state->server, UDP_XACT, SOCKOPT_NONE);
-	state->seq = 0;
-}
-
 void cleanup(void * cookie)
 {
 	state_t *state = (state_t *) cookie;
 
 	close(state->sock);
+	free(state->buf);
 }
 
 void
-server_main()
+server_main(int msize)
 {
-	int     net, sock, sent, namelen, seq = 0;
+	char	*buf = (char*)valloc(msize);
+	int     sock, sent, namelen, seq = 0;
 	struct sockaddr_in it;
 
 	GO_AWAY;
@@ -142,12 +153,12 @@ server_main()
 
 	while (1) {
 		namelen = sizeof(it);
-		if (recvfrom(sock, (void*)&sent, sizeof(sent), 0, 
+		if (recvfrom(sock, (void*)buf, msize, 0, 
 		    (struct sockaddr*)&it, &namelen) < 0) {
 			fprintf(stderr, "lat_udp server: recvfrom: got wrong size\n");
 			exit(9);
 		}
-		sent = ntohl(sent);
+		sent = ntohl(*(int*)buf);
 		if (sent < 0) {
 			udp_done(UDP_XACT);
 			exit(0);
@@ -155,8 +166,8 @@ server_main()
 		if (sent != ++seq) {
 			seq = sent;
 		}
-		net = htonl(seq);
-		if (sendto(sock, (void*)&net, sizeof(net), 0, 
+		*(int*)buf = htonl(seq);
+		if (sendto(sock, (void*)buf, msize, 0, 
 		    (struct sockaddr*)&it, sizeof(it)) < 0) {
 			perror("lat_udp sendto");
 			exit(9);
