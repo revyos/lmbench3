390112ddde35d45ecf1c9dfc19fa51ce8aefe069
hp.com!staelin <hp.com!staelin@9cc621ae-7264-4df2-8f7e-c588375cf588>
2000-06-21 16:28:52 +0000

    - Added '-m <message size>' command line argument - Updated command line parsing to use '-S server' to shutdown server processes
    (Logical change 1.18)

--- trunk/lmbench3/src/bw_tcp.c
+++ trunk/lmbench3/src/bw_tcp.c
@@ -17,51 +17,44 @@ char	*id = "$Id$\n";
 
 typedef struct _state {
 	uint64	move;
-	char	server[256];
+	int	msize;
+	char	*server;
 	int	fd;
 	char	*buf;
 } state_t;
 
-void	server_main(state_t *state);
+void	server_main();
 void	client_main(int parallel, state_t *state);
-void	source(int data, state_t *state);
-void	transfer(uint64 move, char *server, char *buf);
+void	source(int data);
+void	transfer(state_t* state);
 
 void	loop_transfer(uint64 iter, void *cookie);
 
 int main(int ac, char **av)
 {
 	int parallel = 1;
+	int	server = 0;
+	int	shutdown = 0;
 	state_t state;
-	char	*usage = "-s\n OR [-P <parallelism>] server [size]\n OR [-]serverhost\n";
-	char	*server;
+	char	*usage = "-s\n OR [-m <message size>] [-P <parallelism>] server [size]\n OR -S serverhost\n";
 	int	c;
 	uint64	usecs;
 	
-	state.buf = valloc(XFERSIZE);
-	touch(state.buf, XFERSIZE);
+	state.msize = 0;
+	state.move = 10*1024*1024;
 
-	/* Start the server */
-	if (!strcmp(av[1], "-s")) {
-		if (fork() == 0) {
-			server_main(&state);
-		}
-		exit(0);
-	}
-
-	/* Shut down the server */
-	if ((ac == 2) && (av[1][0] == '-')) {
-		int	conn;
-		server = &av[1][1];
-		conn = tcp_connect(server, TCP_DATA, SOCKOPT_NONE);
-		write(conn, "0", 1);
-		exit(0);
-	}
-	
-		
 	/* Rest is client argument processing */
-	while (( c = getopt(ac, av, "P:")) != EOF) {
+	while (( c = getopt(ac, av, "sSm:P:")) != EOF) {
 		switch(c) {
+		case 's': /* Server */
+			server = 1;
+			break;
+		case 'S': /* shutdown serverhost */
+			shutdown = 1;
+			break;
+		case 'm':
+			state.msize = atoi(optarg);
+			break;
 		case 'P':
 			parallel = atoi(optarg);
 			if (parallel <= 0) lmbench_usage(ac, av, usage);
@@ -72,41 +65,50 @@ int main(int ac, char **av)
 		}
 	}
 
-	/*
-	 * Client should have one, possibly two [indicates size] arguments left
-	 */
-	state.move = 10*1024*1024;
-	if (optind + 2 == ac) {
-		state.move = bytes(av[optind+1]);
-		strcpy(state.server,av[optind]);
-	} else if (optind + 1 == ac) {
-		strcpy(state.server,av[optind]);
-	} else {
+	if (server && shutdown
+	    || !server && (optind < ac - 2 || optind >= ac)) {
 		lmbench_usage(ac, av, usage);
 	}
 
+	if (server) {
+		if (fork() == 0) {
+			server_main();
+		}
+		exit(0);
+	}
+
+	state.server = av[optind++];
+	if (optind < ac) {
+		state.move = bytes(av[optind]);
+	}
+	if (state.msize == 0) {
+		state.msize = state.move;
+	}
+	/* make the number of bytes to move a multiple of the message size */
+	if (state.move % state.msize) {
+		state.move += state.move - state.move % state.msize;
+	}
+
+	if (shutdown) {
+		int	conn;
+		int	n = htonl(0);
+		conn = tcp_connect(state.server, TCP_DATA, SOCKOPT_NONE);
+		write(conn, &n, sizeof(int));
+		exit(0);
+	}
+
+	state.buf = valloc(state.msize);
 	if (!state.buf) {
 		perror("valloc");
 		exit(1);
 	}
+	touch(state.buf, state.msize);
 
 	/*
 	 * Make one run take at least 5 seconds.
 	 * This minimizes the effect of connect & reopening TCP windows.
 	 */
-	start(0);
-	transfer(state.move, state.server, state.buf);
-	usecs = stop(0,0);
-	if (usecs >= LONGER) {	/* must be 10Mbit ether or sloooow box */
-		save_n(1);
-		if (parallel == 1)
-			goto out; /* can't skip next step for MP tests! */
-	}
-	usecs = 5000000 / usecs;
-	state.move  *= usecs * 1.25;
-
-	benchmp(NULL, loop_transfer, NULL,
-		LONGER, parallel, &state );
+	benchmp(NULL, loop_transfer, NULL, LONGER, parallel, &state );
 
 out:	(void)fprintf(stderr, "Socket bandwidth using %s: ", state.server);
 	mb(state.move * get_n() * parallel);
@@ -117,29 +119,35 @@ void loop_transfer(uint64 iterations, void *cookie)
 	state_t *state = (state_t *) cookie;
 	uint64	move = state->move;
 	char	*server = state->server;
-	char	*buf    = state->buf;
 
 	while (iterations-- > 0) {
-		transfer(move,server,buf);
+		transfer(state);
 	}
 }
 
-void transfer(uint64 move, char *server, char *buf)
+void transfer(state_t* state)
 {
 	int	data, c;
 	uint64	todo;
+	char	*buf    = state->buf;
 
-	todo = move;
 	/*printf("Move %lu MB\n", (unsigned long)(move>>20)); */
 	
-	data = tcp_connect(server, TCP_DATA, SOCKOPT_READ);
-	(void)sprintf(buf, "%lu", (unsigned long)move);
-	if (write(data, buf, strlen(buf)) != strlen(buf)) {
+	data = tcp_connect(state->server, TCP_DATA, SOCKOPT_READ);
+	c = htonl(state->move);
+	if (write(data, &c, sizeof(int)) != sizeof(int)) {
 		perror("control write");
 		exit(1);
 	}
-	while (todo > 0 && (c = read(data, buf, XFERSIZE)) > 0) {
-		todo -= c;
+	c = htonl(state->msize);
+	if (write(data, &c, sizeof(int)) != sizeof(int)) {
+		perror("control write");
+		exit(1);
+	}
+	for (todo = state->move; todo > 0; todo -= c) {
+		if (c = read(data, state->buf, state->msize) <= 0) {
+			break;
+		}
 	}
 	(void)close(data);
 }
@@ -150,7 +158,7 @@ void child()
 	signal(SIGCHLD, child);
 }
 
-void server_main(state_t *state)
+void server_main()
 {
 	int	data, newdata;
 
@@ -166,7 +174,7 @@ void server_main(state_t *state)
 			perror("fork");
 			break;
 		    case 0:
-			source(newdata, state);
+			source(newdata);
 			exit(0);
 		    default:
 			close(newdata);
@@ -179,20 +187,16 @@ void server_main(state_t *state)
  * Read the number of bytes to be transfered.
  * Write that many bytes on the data socket.
  */
-void source(int data, state_t *state)
+void source(int data)
 {
-	int	n, nbytes;
+	int	tmp, n, m, nbytes;
+	char	*buf;
 
-	if (!state->buf) {
-		perror("valloc");
-		exit(1);
-	}
-	bzero((void*)state->buf, XFERSIZE);
-	if (read(data, state->buf, XFERSIZE) <= 0) {
+	if (read(data, &tmp, sizeof(int)) != sizeof(int)) {
 		perror("control nbytes");
 		exit(7);
 	}
-	nbytes = atoi(state->buf);
+	nbytes = ntohl(tmp);
 
 	/*
 	 * A hack to allow turning off the absorb daemon.
@@ -202,12 +206,27 @@ void source(int data, state_t *state)
 		kill(getppid(), SIGTERM);
 		exit(0);
 	}
+
+	/*
+	 * Now read the message size
+	 */
+	if (read(data, &tmp, sizeof(int)) != sizeof(int)) {
+		perror("control msize");
+		exit(7);
+	}
+	m = ntohl(tmp);
+	buf = valloc(m);
+	bzero(buf, m);
+
 	while (nbytes > 0) {
 #ifdef	TOUCH
-		touch(state->buf, XFERSIZE);
+		touch(buf, m);
 #endif
-		n = write(data, state->buf, XFERSIZE);
-		if (n <= 0) break;
+		n = write(data, buf, m);
+		if (n <= 0) {
+			perror("data write");
+			exit(1);
+		}
 		nbytes -= n;
 	}
 }
