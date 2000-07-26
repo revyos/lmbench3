0bfc273bed4433e6cb0429b4a271bb1dd70a5195
hp.com!staelin <hp.com!staelin@9cc621ae-7264-4df2-8f7e-c588375cf588>
2000-07-26 15:10:05 +0000

    - Rewrite of experimental method; use a single connection for all experiments, with   a single warmup period preceding all measurements.  Previously created a new connection   with a warmup period for each measurement (slow, slow, slow) - Moved initialization and cleanup to new initialize() and cleanup() routines - Only pass msize to server, since it will keep writing data until the connection is   closed.  Previously also passed "move"
    (Logical change 1.36)

--- trunk/lmbench3/src/bw_tcp.c
+++ trunk/lmbench3/src/bw_tcp.c
@@ -6,6 +6,7 @@
  *	client usage:	bw_tcp [-m <message size>] [-P <parallelism>] [-W <warmup>] [-N <repetitions>] hostname [bytes]
  *	shutdown:	bw_tcp -hostname
  *
+ * Copyright (c) 2000 Carl Staelin.
  * Copyright (c) 1994 Larry McVoy.  Distributed under the FSF GPL with
  * additional restriction that results may published only if
  * (1) the benchmark is unmodified, and
@@ -16,7 +17,8 @@ char	*id = "$Id$\n";
 #include "bench.h"
 
 typedef struct _state {
-	uint64	move;
+	int	sock;
+	long	move;
 	int	msize;
 	char	*server;
 	int	fd;
@@ -26,36 +28,46 @@ typedef struct _state {
 void	server_main();
 void	client_main(int parallel, state_t *state);
 void	source(int data);
-void	transfer(state_t* state);
 
+void	initialize(void* cookie);
 void	loop_transfer(uint64 iter, void *cookie);
+void	cleanup(void* cookie);
 
 int main(int ac, char **av)
 {
 	int	parallel = 1;
 	int	warmup = 0;
 	int	repetitions = TRIES;
-	int	server = 0;
 	int	shutdown = 0;
 	state_t state;
-	char	*usage = "-s\n OR [-m <message size>] [-P <parallelism>] [-W <warmup>] [-N <repetitions>] server [size]\n OR -S serverhost\n";
+	char	*usage = "-s\n OR [-m <message size>] [-M <bytes to move>] [-P <parallelism>] [-W <warmup>] [-N <repetitions>] server\n OR -S serverhost\n";
 	int	c;
-	uint64	usecs;
 	
 	state.msize = 0;
-	state.move = 10*1024*1024;
+	state.move = 0;
 
 	/* Rest is client argument processing */
-	while (( c = getopt(ac, av, "sSm:P:W:N:")) != EOF) {
+	while (( c = getopt(ac, av, "sS:m:M:P:W:N:")) != EOF) {
 		switch(c) {
 		case 's': /* Server */
-			server = 1;
+			if (fork() == 0) {
+				server_main();
+			}
+			exit(0);
 			break;
 		case 'S': /* shutdown serverhost */
-			shutdown = 1;
-			break;
+		{
+			int	conn;
+			int	n = htonl(0);
+			conn = tcp_connect(optarg, TCP_DATA, SOCKOPT_NONE);
+			write(conn, &n, sizeof(int));
+			exit(0);
+		}
 		case 'm':
-			state.msize = atoi(optarg);
+			state.msize = bytes(optarg);
+			break;
+		case 'M':
+			state.move = bytes(optarg);
 			break;
 		case 'P':
 			parallel = atoi(optarg);
@@ -73,92 +85,84 @@ int main(int ac, char **av)
 		}
 	}
 
-	if (server && shutdown
-	    || !server && (optind < ac - 2 || optind >= ac)) {
+	if (optind < ac - 2 || optind >= ac) {
 		lmbench_usage(ac, av, usage);
 	}
 
-	if (server) {
-		if (fork() == 0) {
-			server_main();
-		}
-		exit(0);
+	state.server = av[optind++];
+
+	if (state.msize == 0 && state.move == 0) {
+		state.msize = state.move = 10 * 1024 * 1024;
+	} else if (state.msize == 0) {
+		state.msize = state.move;
+	} else if (state.move == 0) {
+		state.move = state.msize;
 	}
 
-	state.server = av[optind++];
-	if (optind < ac) {
-		state.move = bytes(av[optind]);
-	}
-	if (state.msize == 0) {
-		state.msize = state.move;
-	}
 	/* make the number of bytes to move a multiple of the message size */
 	if (state.move % state.msize) {
-		state.move += state.move - state.move % state.msize;
+		state.move += state.msize - state.move % state.msize;
 	}
-
-	if (shutdown) {
-		int	conn;
-		int	n = htonl(0);
-		conn = tcp_connect(state.server, TCP_DATA, SOCKOPT_NONE);
-		write(conn, &n, sizeof(int));
-		exit(0);
-	}
-
-	state.buf = valloc(state.msize);
-	if (!state.buf) {
-		perror("valloc");
-		exit(1);
-	}
-	touch(state.buf, state.msize);
 
 	/*
-	 * Make one run take at least 5 seconds.
-	 * This minimizes the effect of connect & reopening TCP windows.
+	 * Initialize connection by running warming up for at least
+	 * five seconds, then measure performance over one second.
+	 * This minimizes the effect of opening and initializing TCP 
+	 * connections.
 	 */
-	benchmp(NULL, loop_transfer, NULL, LONGER, parallel, warmup, repetitions, &state );
-
-out:	(void)fprintf(stderr, "Socket bandwidth using %s: ", state.server);
+	benchmp(initialize, loop_transfer, cleanup, 
+		SHORT, parallel, LONGER + warmup, repetitions, &state );
+	/* (void)fprintf(stderr, "Socket bandwidth using %s\n", state.server); */
+	fprintf(stderr, "%.6f ", state.msize / (1000. * 1000.));
 	mb(state.move * get_n() * parallel);
 }
 
-void loop_transfer(uint64 iterations, void *cookie)
+void
+initialize(void *cookie)
 {
+	int	c;
 	state_t *state = (state_t *) cookie;
-	uint64	move = state->move;
-	char	*server = state->server;
+
+	state->buf = valloc(state->msize);
+	if (!state->buf) {
+		perror("valloc");
+		exit(1);
+	}
+	touch(state->buf, state->msize);
+
+	state->sock = tcp_connect(state->server, TCP_DATA, SOCKOPT_READ);
+	c = htonl(state->msize);
+	if (write(state->sock, &c, sizeof(int)) != sizeof(int)) {
+		perror("control write");
+		exit(1);
+	}
+}
+
+void 
+loop_transfer(uint64 iterations, void *cookie)
+{
+	int	c;
+	long	todo;
+	state_t *state = (state_t *) cookie;
 
 	while (iterations-- > 0) {
-		transfer(state);
-	}
-}
-
-void transfer(state_t* state)
-{
-	int	data, c;
-	uint64	todo;
-	char	*buf    = state->buf;
-
-	/*printf("Move %lu MB\n", (unsigned long)(move>>20)); */
-	
-	data = tcp_connect(state->server, TCP_DATA, SOCKOPT_READ);
-	c = htonl(state->move);
-	if (write(data, &c, sizeof(int)) != sizeof(int)) {
-		perror("control write");
-		exit(1);
-	}
-	c = htonl(state->msize);
-	if (write(data, &c, sizeof(int)) != sizeof(int)) {
-		perror("control write");
-		exit(1);
-	}
-	for (todo = state->move; todo > 0; todo -= c) {
-		if (c = read(data, state->buf, state->msize) <= 0) {
-			break;
+		for (todo = state->move; todo > 0; todo -= c) {
+			if ((c = read(state->sock, state->buf, state->msize)) <= 0) {
+				exit(1);
+			}
 		}
 	}
-	(void)close(data);
 }
+
+void
+cleanup(void* cookie)
+{
+	state_t *state = (state_t *) cookie;
+
+	/* close connection */
+	(void)close(state->sock);
+}
+
 
 void child()
 {
@@ -200,42 +204,34 @@ void source(int data)
 	int	tmp, n, m, nbytes;
 	char	*buf;
 
+	/*
+	 * read the message size
+	 */
 	if (read(data, &tmp, sizeof(int)) != sizeof(int)) {
 		perror("control nbytes");
 		exit(7);
 	}
-	nbytes = ntohl(tmp);
+	m = ntohl(tmp);
 
 	/*
 	 * A hack to allow turning off the absorb daemon.
 	 */
-     	if (nbytes == 0) {
+     	if (m == 0) {
 		tcp_done(TCP_DATA);
 		kill(getppid(), SIGTERM);
 		exit(0);
 	}
 
-	/*
-	 * Now read the message size
-	 */
-	if (read(data, &tmp, sizeof(int)) != sizeof(int)) {
-		perror("control msize");
-		exit(7);
-	}
-	m = ntohl(tmp);
 	buf = valloc(m);
 	bzero(buf, m);
 
-	while (nbytes > 0) {
+	/*
+	 * Keep sending messages until the connection is closed
+	 */
+	while (write(data, buf, m) > 0) {
 #ifdef	TOUCH
 		touch(buf, m);
 #endif
-		n = write(data, buf, m);
-		if (n <= 0) {
-			perror("data write");
-			exit(1);
-		}
-		nbytes -= n;
 	}
 	free(buf);
 }
