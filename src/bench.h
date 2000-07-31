32dd47e0c56163dbe53c039022e754d6688606b2
hp.com!staelin <hp.com!staelin@9cc621ae-7264-4df2-8f7e-c588375cf588>
2000-07-31 08:31:21 +0000

    - Add line for print_results()
    (Logical change 1.44)

--- src/bench.h
+++ src/bench.h
@@ -127,6 +127,7 @@ void	save_median();
 void	save_minimum();
 void	set_results(result_t *r);
 result_t* get_results();
+void	print_results();
 
 
 #define	BENCHO(loop_body, overhead_body, enough) { 			\
