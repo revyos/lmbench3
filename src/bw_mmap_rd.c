381b35ebf5c813a1aaf44c51c809b1b975d66ca1
hp.com!staelin <hp.com!staelin@9cc621ae-7264-4df2-8f7e-c588375cf588>
2002-09-20 06:34:02 +0000

    - switch to using new bread() utility to read data into CPU
    (Logical change 1.195)

--- src/bw_mmap_rd.c
+++ src/bw_mmap_rd.c
@@ -32,8 +32,7 @@ typedef struct _state {
 	char	filename[256];
 	int	fd;
 	int	clone;
-	TYPE	*buf;
-	TYPE	*lastone;
+	void	*buf;
 } state_t;
 
 void time_no_open(iter_t iterations, void * cookie);
@@ -46,7 +45,7 @@ int main(int ac, char **av)
 {
 	int	fd, nbytes;
 	struct	stat sbuf;
-	TYPE	*buf, *lastone;
+	void	*buf;
 	int	parallel = 1;
 	int	warmup = 0;
 	int	repetitions = TRIES;
@@ -136,58 +135,26 @@ init_open(void *cookie)
 
 	initialize(cookie);
 	CHK(state->fd = open(state->filename, 0));
-	CHK(state->buf = (TYPE*)mmap(0, state->nbytes, PROT_READ,
+	CHK(state->buf = mmap(0, state->nbytes, PROT_READ,
 				     MMAP_FLAGS, state->fd, 0));
-	state->lastone = (TYPE*)((char*)state->buf + state->nbytes - MINSZ);
 }
 
 void
 cleanup(void *cookie)
 {
 	state_t *state = (state_t *) cookie;
-	if (state->buf) munmap((void*)state->buf, state->nbytes);
+	if (state->buf) munmap(state->buf, state->nbytes);
 	if (state->fd >= 0) close(state->fd);
 	if (state->clone) unlink(state->filename);
-}
-
-int
-doit(register TYPE *p, register TYPE *lastone)
-{
-	register int sum = 0;
-	while (p <= lastone) {
-		sum += p[0]+p[1]+p[2]+p[3]+p[4]+p[5]+p[6]+p[7]+p[8]+
-		    p[9]+p[10]+p[11]+p[12]+p[13]+p[14]+p[15]+p[16]+p[17]+
-		    p[18]+p[19]+p[20]+p[21]+p[22]+p[23]+p[24]+p[25]+p[26]+
-		    p[27]+p[28]+p[29]+p[30]+p[31]+p[32]+p[33]+p[34]+p[35]+
-		    p[36]+p[37]+p[38]+p[39]+p[40]+p[41]+p[42]+p[43]+
-		    p[44]+p[45]+p[46]+p[47]+p[48]+p[49]+p[50]+p[51]+
-		    p[52]+p[53]+p[54]+p[55]+p[56]+p[57]+p[58]+p[59]+
-		    p[60]+p[61]+p[62]+p[63]+p[64]+p[65]+p[66]+p[67]+
-		    p[68]+p[69]+p[70]+p[71]+p[72]+p[73]+p[74]+p[75]+
-		    p[76]+p[77]+p[78]+p[79]+p[80]+p[81]+p[82]+p[83]+
-		    p[84]+p[85]+p[86]+p[87]+p[88]+p[89]+p[90]+p[91]+
-		    p[92]+p[93]+p[94]+p[95]+p[96]+p[97]+p[98]+p[99]+
-		    p[100]+p[101]+p[102]+p[103]+p[104]+p[105]+p[106]+
-		    p[107]+p[108]+p[109]+p[110]+p[111]+p[112]+p[113]+
-		    p[114]+p[115]+p[116]+p[117]+p[118]+p[119]+p[120]+
-		    p[121]+p[122]+p[123]+p[124]+p[125]+p[126]+p[127];
-		p += 128;
-	}
-	return sum;
 }
 
 void time_no_open(iter_t iterations, void * cookie)
 {
 	state_t *state = (state_t *) cookie;
-	register TYPE *p = state->buf;
-	register TYPE *lastone;
-	register int sum = 0;
 
-	lastone = state->lastone;
 	while (iterations-- > 0) {
-	    sum += doit(p,lastone);
+	    bread(state->buf, state->nbytes);
 	}
-	use_int(sum);
 }
 
 void time_with_open(iter_t iterations, void *cookie)
@@ -196,16 +163,13 @@ void time_with_open(iter_t iterations, void *cookie)
 	char 	*filename = state->filename;
 	int	nbytes    = state->nbytes;
 	int 	fd;
-	register TYPE *p, *lastone;
-	register int sum = 0;
+	void	*p;
 
 	while (iterations-- > 0) {
 	    CHK(fd = open(filename, 0));
-	    CHK(p = (TYPE*)mmap(0, nbytes, PROT_READ, MMAP_FLAGS, fd, 0));
-	    lastone = (TYPE*)((char*)p + nbytes - MINSZ);
-	    sum += doit(p,lastone);
+	    CHK(p = mmap(0, nbytes, PROT_READ, MMAP_FLAGS, fd, 0));
+	    bread(p, nbytes);
 	    close(fd);
-	    munmap((void*)p, nbytes);
+	    munmap(p, nbytes);
 	}
-	use_int(sum);
 }
--- src/lat_ctx.c
+++ src/lat_ctx.c
@@ -26,7 +26,6 @@ int	ncpus;
 #define	max(a, b)	((a) > (b) ? (a) : (b))
 #endif
 
-int	sumit(int*, int);
 void	doit(int **p, int rd, int wr, int process_size);
 int	create_pipes(int **p, int procs);
 int	create_daemons(int **p, pid_t *pids, int procs, int process_size);
@@ -43,7 +42,7 @@ struct _state {
 	int	procs;
 	pid_t*	pids;
 	int	**p;
-	int*	data;
+	void*	data;
 };
 
 int
@@ -154,7 +153,7 @@ initialize_overhead(void* cookie)
 
 	pState->data = (pState->process_size > 0) ? malloc(pState->process_size) : NULL;
 	if (pState->data)
-		bzero((void*)pState->data, pState->process_size);
+		bzero(pState->data, pState->process_size);
 
 	procs = create_pipes(pState->p, pState->procs);
 	if (procs < pState->procs) {
@@ -184,7 +183,6 @@ benchmark_overhead(iter_t iterations, void* cookie)
 	struct _state* pState = (struct _state*)cookie;
 	int	i = 0;
 	int	msg = 1;
-	int	sum = 0;
 
 	while (iterations-- > 0) {
 		if (write(pState->p[i][1], &msg, sizeof(msg)) != sizeof(msg)) {
@@ -198,9 +196,8 @@ benchmark_overhead(iter_t iterations, void* cookie)
 		if (++i == pState->procs) {
 			i = 0;
 		}
-		sum += sumit(pState->data, pState->process_size);
+		bread(pState->data, pState->process_size);
 	}
-	use_int(sum);
 }
 
 void 
@@ -249,7 +246,6 @@ benchmark(iter_t iterations, void* cookie)
 {
 	struct _state* pState = (struct _state*)cookie;
 	int	msg;
-	int	sum = 0;
 
 	/*
 	 * Main process - all others should be ready to roll, time the
@@ -265,35 +261,33 @@ benchmark(iter_t iterations, void* cookie)
 			perror("read/write on pipe");
 			exit(1);
 		}
-		sum += sumit(pState->data, pState->process_size);
+		bread(pState->data, pState->process_size);
 	}
-	use_int(sum);
 }
 
 
 void
 doit(int **p, int rd, int wr, int process_size)
 {
-	int	msg, sum = 0 /* lint */;
-	int*	data = NULL;
+	int	msg;
+	void*	data = NULL;
 
 	signal(SIGTERM, SIG_DFL);
 	if (process_size) {
 		data = malloc(process_size);
-		if (data) bzero((void*)data, process_size);
+		if (data) bzero(data, process_size);
 	}
 	for ( ;; ) {
 		if (read(p[rd][0], &msg, sizeof(msg)) != sizeof(msg)) {
 			perror("read/write on pipe");
 			break;
 		}
-		sum = sumit(data, process_size);
+		bread(data, process_size);
 		if (write(p[wr][1], &msg, sizeof(msg)) != sizeof(msg)) {
 			perror("read/write on pipe");
 			break;
 		}
 	}
-	use_int(sum);
 	exit(1);
 }
 
@@ -355,47 +349,3 @@ create_pipes(int **p, int procs)
 	}
 	return procs;
 }
-
-/*
- * Bring howmuch data into the cache, assuming that the smallest cache
- * line is 16 bytes.
- */
-int
-sumit(int* data, int howmuch)
-{
-	int	done, sum = 0;
-	register int *d = data;
-
-#if 0
-#define	A	sum+=d[0]+d[4]+d[8]+d[12]+d[16]+d[20]+d[24]+d[28]+\
-		d[32]+d[36]+d[40]+d[44]+d[48]+d[52]+d[56]+d[60]+\
-		d[64]+d[68]+d[72]+d[76]+d[80]+d[84]+d[88]+d[92]+\
-		d[96]+d[100]+d[104]+d[108]+d[112]+d[116]+d[120]+d[124];\
-		d+=128;
-#define	TWOKB	A A A A
-#else
-#define	A	sum+=d[0]+d[1]+d[2]+d[3]+d[4]+d[5]+d[6]+d[7]+d[8]+d[9]+\
-		d[10]+d[11]+d[12]+d[13]+d[14]+d[15]+d[16]+d[17]+d[18]+d[19]+\
-		d[20]+d[21]+d[22]+d[23]+d[24]+d[25]+d[26]+d[27]+d[28]+d[29]+\
-		d[30]+d[31]+d[32]+d[33]+d[34]+d[35]+d[36]+d[37]+d[38]+d[39]+\
-		d[40]+d[41]+d[42]+d[43]+d[44]+d[45]+d[46]+d[47]+d[48]+d[49]+\
-		d[50]+d[51]+d[52]+d[53]+d[54]+d[55]+d[56]+d[57]+d[58]+d[59]+\
-		d[60]+d[61]+d[62]+d[63]+d[64]+d[65]+d[66]+d[67]+d[68]+d[69]+\
-		d[70]+d[71]+d[72]+d[73]+d[74]+d[75]+d[76]+d[77]+d[78]+d[79]+\
-		d[80]+d[81]+d[82]+d[83]+d[84]+d[85]+d[86]+d[87]+d[88]+d[89]+\
-		d[90]+d[91]+d[92]+d[93]+d[94]+d[95]+d[96]+d[97]+d[98]+d[99]+\
-		d[100]+d[101]+d[102]+d[103]+d[104]+\
-		d[105]+d[106]+d[107]+d[108]+d[109]+\
-		d[110]+d[111]+d[112]+d[113]+d[114]+\
-		d[115]+d[116]+d[117]+d[118]+d[119]+\
-		d[120]+d[121]+d[122]+d[123]+d[124]+d[125]+d[126]+d[127];\
-		d+=128;	/* ints; bytes == 512 */
-#define	TWOKB	A A A A
-#endif
-
-	for (done = 0; done < howmuch; done += 2048) {
-		TWOKB
-	}
-	return (sum);
-}
-
