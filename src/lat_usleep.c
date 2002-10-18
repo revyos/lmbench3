13651a73153f338691d3eb3077592a2edee883f0
hp.com!staelin <hp.com!staelin@9cc621ae-7264-4df2-8f7e-c588375cf588>
2002-10-18 08:04:52 +0000

    lat_usleep.c:   - add support for more sleep interfaces, namely nanosleep, select     and pselect   - also add ability to specify realtime scheduling (if run as root)
    (Logical change 1.213)

--- src/lat_usleep.c
+++ src/lat_usleep.c
@@ -1,9 +1,9 @@
 /*
  * lat_usleep.c - usleep duration/latency
  *
- * The interval API for usleep(3) and the getitimer(2) and 
- * setitimer(2) interval timers support resolutions down to 
- * a micro-second.  However, implementations do not support 
+ * The APIs for usleep(3), nanosleep(2), select(2), pselect(2),
+ * getitimer(2) and setitimer(2) support resolutions down to 
+ * a micro-second.  However, many implementations do not support 
  * such resolution.  Most current implementations (as of Fall 
  * 2002) simply put the process back on the run queue and the 
  * process may get run on the next scheduler time slice (10-20 
@@ -29,20 +29,70 @@
 char           *id = "$Id$\n";
 
 #include "bench.h"
+#include <sched.h>
+#include <sys/select.h>
 
-typedef     enum {ITIMER, USLEEP} timer_e;
+typedef     enum {USLEEP, NANOSLEEP, SELECT, PSELECT, ITIMER} timer_e;
 
 uint64          caught,
                 n;
 struct itimerval value;
 
+typedef struct _state {
+    unsigned long usecs;
+} state_t;
+
 void
 bench_usleep(iter_t iterations, void *cookie)
 {
-    unsigned long  *usecs = (unsigned long *) cookie;
+    state_t        *state = (state_t*)cookie;
 
     while (iterations-- > 0) {
-	usleep(*usecs);
+	usleep(state->usecs);
+    }
+}
+
+void
+bench_nanosleep(iter_t iterations, void *cookie)
+{
+    state_t        *state = (state_t*)cookie;
+    struct timespec req;
+    struct timespec rem;
+
+    req.tv_sec = 0;
+    req.tv_nsec = state->usecs * 1000;
+
+    while (iterations-- > 0) {
+	if (nanosleep(&req, &rem) < 0) {
+	    while (nanosleep(&rem, &rem) < 0)
+		;
+	}
+    }
+}
+
+void
+bench_select(iter_t iterations, void *cookie)
+{
+    state_t        *state = (state_t*)cookie;
+    struct timeval  tv;
+
+    while (iterations-- > 0) {
+	tv.tv_sec = 0;
+	tv.tv_usec = state->usecs;
+	select(0, NULL, NULL, NULL, &tv);
+    }
+}
+
+void
+bench_pselect(iter_t iterations, void *cookie)
+{
+    state_t        *state = (state_t*)cookie;
+    struct timespec ts;
+
+    while (iterations-- > 0) {
+	ts.tv_sec = 0;
+	ts.tv_nsec = state->usecs * 1000;
+	pselect(0, NULL, NULL, NULL, &ts, NULL);
     }
 }
 
@@ -60,13 +110,13 @@ interval()
 void
 initialize(void *cookie)
 {
-    unsigned long  *usecs = (unsigned long *) cookie;
+    state_t        *state = (state_t*)cookie;
     struct sigaction sa;
 
     value.it_interval.tv_sec = 0;
-    value.it_interval.tv_usec = *usecs;
+    value.it_interval.tv_usec = state->usecs;
     value.it_value.tv_sec = 0;
-    value.it_value.tv_usec = *usecs;
+    value.it_value.tv_usec = state->usecs;
 
     sa.sa_handler = interval;
     sigemptyset(&sa.sa_mask);
@@ -95,24 +145,48 @@ bench_itimer(iter_t iterations, void *cookie)
     }
 }
 
+void
+set_realtime()
+{
+    struct sched_param sp;
+
+    sp.sched_priority = sched_get_priority_max(SCHED_RR);
+    sched_setscheduler(0, SCHED_RR, &sp);
+}
+
 int
 main(int ac, char **av)
 {
+    int             realtime = 0;
     int             warmup = 0;
     int             repetitions = TRIES;
     int             c;
     char            buf[512];
     timer_e	    what = USLEEP;
-    char           *usage = "[-u|i] [-W <warmup>] [-N <repetitions>] usecs\n";
-    unsigned long   usecs;
+    state_t         state;
+    char           *usage = "[-r] [-u <method>] [-W <warmup>] [-N <repetitions>] usecs\nmethod=usleep|nanosleep|select|pselect|itimer\n";
 
-    while ((c = getopt(ac, av, "iuW:N:")) != EOF) {
+    realtime = 0;
+
+    while ((c = getopt(ac, av, "ru:W:N:")) != EOF) {
 	switch (c) {
-	case 'i':
-	    what = ITIMER;
+	case 'r':
+	    realtime = 1;
 	    break;
 	case 'u':
-	    what = USLEEP;
+	    if (strcmp(optarg, "usleep") == 0) {
+		what = USLEEP;
+	    } else if (strcmp(optarg, "nanosleep") == 0) {
+		what = NANOSLEEP;
+	    } else if (strcmp(optarg, "select") == 0) {
+		what = SELECT;
+	    } else if (strcmp(optarg, "pselect") == 0) {
+		what = PSELECT;
+	    } else if (strcmp(optarg, "itimer") == 0) {
+		what = ITIMER;
+	    } else {
+		lmbench_usage(ac, av, usage);
+	    }
 	    break;
 	case 'W':
 	    warmup = atoi(optarg);
@@ -129,17 +203,34 @@ main(int ac, char **av)
 	lmbench_usage(ac, av, usage);
     }
 
-    usecs = bytes(av[optind]);
+    state.usecs = bytes(av[optind]);
+    if (realtime) set_realtime();
+
     switch (what) {
     case USLEEP:
 	benchmp(NULL, bench_usleep, NULL, 
-		0, 1, warmup, repetitions, &usecs);
-	sprintf(buf, "usleep %lu microseconds", usecs);
+		0, 1, warmup, repetitions, &state);
+	sprintf(buf, "usleep %lu microseconds", state.usecs);
+	break;
+    case NANOSLEEP:
+	benchmp(NULL, bench_nanosleep, NULL, 
+		0, 1, warmup, repetitions, &state);
+	sprintf(buf, "nanosleep %lu microseconds", state.usecs);
+	break;
+    case SELECT:
+	benchmp(NULL, bench_select, NULL, 
+		0, 1, warmup, repetitions, &state);
+	sprintf(buf, "select %lu microseconds", state.usecs);
+	break;
+    case PSELECT:
+	benchmp(NULL, bench_pselect, NULL, 
+		0, 1, warmup, repetitions, &state);
+	sprintf(buf, "pselect %lu microseconds", state.usecs);
 	break;
     case ITIMER:
 	benchmp(initialize, bench_itimer, NULL, 
-		0, 1, warmup, repetitions, &usecs);
-	sprintf(buf, "itimer %lu microseconds", usecs);
+		0, 1, warmup, repetitions, &state);
+	sprintf(buf, "itimer %lu microseconds", state.usecs);
 	break;
     default:
 	lmbench_usage(ac, av, usage);
