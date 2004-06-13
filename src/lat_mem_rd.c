aa2206f25cc33251d93d56757721e35c67089ecd
hp.com!staelin <hp.com!staelin@9cc621ae-7264-4df2-8f7e-c588375cf588>
2004-06-13 08:03:49 +0000

    - Move memory pointer chain initialization from lat_mem_rd to lib_mem
    (Logical change 1.269)

--- src/lat_mem_rd.c
+++ src/lat_mem_rd.c
@@ -22,127 +22,7 @@ void	loads(size_t len, size_t range, size_t stride,
 size_t	step(size_t k);
 void	initialize(iter_t iterations, void* cookie);
 
-benchmp_f	fpInit = initialize;
-
-void
-base_initialize(iter_t iterations, void* cookie)
-{
-	int	nwords, nlines, nbytes, npages, nmpages;
-	size_t *pages;
-	size_t *lines;
-	size_t *words;
-	struct mem_state* state = (struct mem_state*)cookie;
-	register char *p = 0 /* lint */;
-
-	if (iterations) return;
-
-	state->initialized = 0;
-
-	state->maxlen = state->len;
-	nbytes = state->len;
-	nwords = state->line / sizeof(char*);
-	nlines = state->pagesize / state->line;
-	npages = (nbytes + state->pagesize - 1) / state->pagesize;
-	nmpages= (state->maxlen + state->pagesize - 1) / state->pagesize;
-
-	srand(getpid());
-
-	words = NULL;
-	lines = NULL;
-	pages = permutation(nmpages, state->pagesize);
-	p = state->addr = (char*)malloc(state->maxlen + 2 * state->pagesize);
-
-	state->nwords = nwords;
-	state->nlines = nlines;
-	state->npages = npages;
-	state->lines = lines;
-	state->pages = pages;
-	state->words = words;
-
-	if (state->addr == NULL) {
-		return;
-	}
-
-	if ((unsigned long)p % state->pagesize) {
-		p += state->pagesize - (unsigned long)p % state->pagesize;
-	}
-	state->base = p;
-	state->initialized = 1;
-}
-
-void
-initialize(iter_t iterations, void* cookie)
-{
-	struct mem_state* state = (struct mem_state*)cookie;
-	size_t	i;
-	size_t	range = state->len;
-	size_t	stride = state->line;
-	char*	addr;
-
-	base_initialize(iterations, cookie);
-	addr = state->base;
-
-	/*
-	 * Create a circular list of pointers using a simple striding
-	 * algorithm.  
-	 * 
-	 * This access pattern corresponds to many array/matrix
-	 * algorithms.  It should be easily and correctly predicted
-	 * by any decent hardware prefetch algorithm.
-	 */
-	for (i = stride; i < range; i += stride) {
-		*(char **)&addr[i - stride] = (char*)&addr[i];
-	}
-	*(char **)&addr[i - stride] = (char*)&addr[0];
-	state->p[0] = addr;
-}
-
-void
-initialize_thrash(iter_t iterations, void* cookie)
-{
-	struct mem_state* state = (struct mem_state*)cookie;
-	int	npages = (state->len + state->pagesize - 1) / state->pagesize;
-	size_t *pages;
-	size_t	i;
-	size_t	j;
-	size_t	cur;
-	size_t	next;
-	size_t	start;
-	size_t	range = state->len;
-	size_t	stride = state->line;
-	char*	addr;
-
-	base_initialize(iterations, cookie);
-	addr = state->base;
-
-	/*
-	 * Create a circular list of pointers with a random access
-	 * pattern.
-	 *
-	 * This stream corresponds more closely to linked list
-	 * memory access patterns.  For large data structures each
-	 * access will likely cause both a cache miss and a TLB miss.
-	 * 
-	 * Access a different page each time.  This will eventually
-	 * cause a tlb miss each page.  It will also cause maximal
-	 * thrashing in the cache between the user data stream and
-	 * the page table entries.
-	 */
-	pages = state->pages;
-	cur = pages[0];
-	for (i = 0; i < state->nlines; ++i) {
-	    for (j = 0; j < state->npages; ++j) {
-		if (j < state->npages - 1) {
-		    next = pages[j + 1] + ((i + j + 1) % state->nlines) * state->line;
-		} else {
-		    next = pages[0] + ((i + 1) % state->nlines) * state->line;
-		}
-		*(char **)&addr[cur] = (char*)&addr[next];
-		cur = next;
-	    }
-	}
-	state->p[0] = (char*)&addr[pages[0]];
-}
+benchmp_f	fpInit = stride_initialize;
 
 int
 main(int ac, char **av)
@@ -160,7 +40,7 @@ main(int ac, char **av)
 	while (( c = getopt(ac, av, "tP:W:N:")) != EOF) {
 		switch(c) {
 		case 't':
-			fpInit = initialize_thrash;
+			fpInit = thrash_initialize;
 			break;
 		case 'P':
 			parallel = atoi(optarg);
@@ -181,7 +61,8 @@ main(int ac, char **av)
 		lmbench_usage(ac, av, usage);
 	}
 
-        len = atoi(av[optind]) * 1024 * 1024;
+        len = atoi(av[optind]);
+	len *= 1024 * 1024;
 
 	if (optind == ac - 1) {
 		fprintf(stderr, "\"stride=%d\n", STRIDE);
@@ -273,15 +154,15 @@ step(size_t k)
 		k = k * 2;
         } else if (k < 4*1024) {
 		k += 1024;
-        } else if (k < 32*1024) {
+	} else if (k < 32*1024) {
 		k += 2048;
-        } else if (k < 64*1024) {
+	} else if (k < 64*1024) {
 		k += 4096;
-        } else if (k < 128*1024) {
+	} else if (k < 128*1024) {
 		k += 8192;
-        } else if (k < 256*1024) {
+	} else if (k < 256*1024) {
 		k += 16384;
-        } else if (k < 512*1024) {
+	} else if (k < 512*1024) {
 		k += 32*1024;
 	} else if (k < 4<<20) {
 		k += 512 * 1024;
@@ -289,7 +170,7 @@ step(size_t k)
 		k += 1<<20;
 	} else if (k < 20<<20) {
 		k += 2<<20;
-	} else {
+ 	} else {
 		k += 10<<20;
 	}
 	return (k);
