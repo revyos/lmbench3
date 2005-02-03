422dec49aa397b591a7070a3d6a9b429862e2a0f
hp.com!staelin <hp.com!staelin@9cc621ae-7264-4df2-8f7e-c588375cf588>
2005-02-03 15:21:50 +0000

    - Cleanup handling of unused pipe file descriptors (close them early)
    (Logical change 1.286)

--- trunk/lmbench3/src/lat_ctx.c
+++ trunk/lmbench3/src/lat_ctx.c
@@ -26,7 +26,7 @@ int	ncpus;
 #define	max(a, b)	((a) > (b) ? (a) : (b))
 #endif
 
-void	doit(int **p, int rd, int wr, int process_size);
+void	doit(int rd, int wr, int process_size);
 int	create_pipes(int **p, int procs);
 int	create_daemons(int **p, pid_t *pids, int procs, int process_size);
 void	initialize_overhead(iter_t iterations, void* cookie);
@@ -190,11 +190,11 @@ benchmark_overhead(iter_t iterations, void* cookie)
 
 	while (iterations-- > 0) {
 		if (write(pState->p[i][1], &msg, sizeof(msg)) != sizeof(msg)) {
-			perror("read/write on pipe");
+			/* perror("read/write on pipe"); */
 			exit(1);				
 		}
 		if (read(pState->p[i][0], &msg, sizeof(msg)) != sizeof(msg)) {
-			perror("read/write on pipe");
+			/* perror("read/write on pipe"); */
 			exit(1);
 		}
 		if (++i == pState->procs) {
@@ -237,6 +237,7 @@ cleanup(iter_t iterations, void* cookie)
 	/*
 	 * Close the pipes and kill the children.
 	 */
+	cleanup_overhead(iterations, cookie);
      	for (i = 1; pState->pids && i < pState->procs; ++i) {
 		if (pState->pids[i] > 0) {
 			kill(pState->pids[i], SIGKILL);
@@ -246,7 +247,6 @@ cleanup(iter_t iterations, void* cookie)
 	if (pState->pids)
 		free(pState->pids);
 	pState->pids = NULL;
-	cleanup_overhead(iterations, cookie);
 }
 
 void
@@ -262,11 +262,11 @@ benchmark(iter_t iterations, void* cookie)
 	while (iterations-- > 0) {
 		if (write(pState->p[0][1], &msg, sizeof(msg)) !=
 		    sizeof(msg)) {
-			perror("read/write on pipe");
+			/* perror("read/write on pipe"); */
 			exit(1);
 		}
 		if (read(pState->p[pState->procs-1][0], &msg, sizeof(msg)) != sizeof(msg)) {
-			perror("read/write on pipe");
+			/* perror("read/write on pipe"); */
 			exit(1);
 		}
 		bread(pState->data, pState->process_size);
@@ -275,24 +275,23 @@ benchmark(iter_t iterations, void* cookie)
 
 
 void
-doit(int **p, int rd, int wr, int process_size)
+doit(int rd, int wr, int process_size)
 {
 	int	msg;
 	void*	data = NULL;
 
-	signal(SIGTERM, SIG_DFL);
 	if (process_size) {
 		data = malloc(process_size);
 		if (data) bzero(data, process_size);
 	}
 	for ( ;; ) {
-		if (read(p[rd][0], &msg, sizeof(msg)) != sizeof(msg)) {
-			perror("read/write on pipe");
+		if (read(rd, &msg, sizeof(msg)) != sizeof(msg)) {
+			/* perror("read/write on pipe"); */
 			break;
 		}
 		bread(data, process_size);
-		if (write(p[wr][1], &msg, sizeof(msg)) != sizeof(msg)) {
-			perror("read/write on pipe");
+		if (write(wr, &msg, sizeof(msg)) != sizeof(msg)) {
+			/* perror("read/write on pipe"); */
 			break;
 		}
 	}
@@ -303,7 +302,7 @@ doit(int **p, int rd, int wr, int process_size)
 int
 create_daemons(int **p, pid_t *pids, int procs, int process_size)
 {
-	int	i;
+	int	i, j;
 	int	msg;
 
 	/*
@@ -321,8 +320,11 @@ create_daemons(int **p, pid_t *pids, int procs, int process_size)
 #if	defined(sgi) && defined(PIN)
 			sysmp(MP_MUSTRUN, i % ncpus);
 #endif
-			signal(SIGTERM, exit);
-			doit(p, i-1, i, process_size);
+			for (j = 0; j < procs; ++j) {
+				if (j != i - 1) close(p[j][0]);
+				if (j != i) close(p[j][1]);
+			}
+			doit(p[i-1][0], p[i][1], process_size);
 			/* NOTREACHED */
 
 		    default:	/* parent */
@@ -336,7 +338,7 @@ create_daemons(int **p, pid_t *pids, int procs, int process_size)
 	 */
 	if (write(p[0][1], &msg, sizeof(msg)) != sizeof(msg) ||
 	    read(p[procs-1][0], &msg, sizeof(msg)) != sizeof(msg)) {
-		perror("write/read/write on pipe");
+		/* perror("write/read/write on pipe"); */
 		exit(1);
 	}
 	return procs;
