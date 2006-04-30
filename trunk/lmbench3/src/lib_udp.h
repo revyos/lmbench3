ac2806fa0a36bd08c848fdb0a8b9aa93f47aa09f
staelin <staelin@9cc621ae-7264-4df2-8f7e-c588375cf588>
2006-04-30 07:22:05 +0000

    - make type of 'prog' u_long to be consistent

--- trunk/lmbench3/src/lib_udp.h
+++ trunk/lmbench3/src/lib_udp.h
@@ -5,7 +5,7 @@
 #include	<arpa/inet.h>
 
 int	udp_server(u_long prog, int rdwr);
-void	udp_done(int prog);
+void	udp_done(u_long prog);
 int	udp_connect(char *host, u_long prog, int rdwr);
 void	sock_optimize(int sock, int rdwr);
 int	sockport(int);
